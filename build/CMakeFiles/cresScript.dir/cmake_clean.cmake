e879c58882da31b26737e4cc7d0a3ef5e02025b3
Oiltanker <oiltankerdk@gmail.com>
2019-04-27 00:29:44 +0200

    gitignore addition

--- build/CMakeFiles/cresScript.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cresScript"
-  "../bin/CmakeResources.cmake"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cresScript.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
