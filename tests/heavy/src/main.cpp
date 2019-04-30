06847b8e850d16a4b399ce9b01652c0515b796f8
Oiltanker <oiltankerdk@gmail.com>
2019-04-30 02:29:14 +0200

    issue #3

--- tests/heavy/src/main.cpp
+++ tests/heavy/src/main.cpp
@@ -1,23 +1,22 @@
 #include "resources.h"
 
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <algorithm>
+#include <functional>
+#include <string>
 
 using namespace std;
 using namespace rc;
 
-void printAsString(ofstream& outFile, const Resource& rc) {
-    char buf[1025];
-    size_t curPos = 0;
-    for (size_t i = 0; i < rc.size; i++) {
-        for (curPos = 0; (curPos < 1024) && (i < rc.size); curPos++, i++) {
-            buf[curPos] = ((const char*)rc.begin)[i];
-        }
-        buf[curPos] = '\0';
-        outFile << buf;
+void printAsHash(ofstream& outFile, const Resource& rc) {
+    hash<string> str_hash;
+    for (size_t i = 0; i < rc.size;) {
+        size_t step = min(rc.size - i, 10240ULL);
+        string str(((char*)rc.begin)[i], step);
+        outFile << str_hash(str) << "\n";
+        i += step;
     }
-    outFile << "\n";
 }
 
 int main(int argc, char** argv) {
@@ -35,13 +34,13 @@ int main(int argc, char** argv) {
     const Resource& r4 = R.bundle._3and4._4._4_txt;
 
     outFile << "RES: 'R.bundle._1and2._1_txt':\n";
-    printAsString(outFile, r1);
+    printAsHash(outFile, r1);
     outFile << "RES: 'R.bundle._1and2._2_txt':\n";
-    printAsString(outFile, r2);
+    printAsHash(outFile, r2);
     outFile << "RES: 'R.bundle._3and4._3_txt':\n";
-    printAsString(outFile, r3);
+    printAsHash(outFile, r3);
     outFile << "RES: 'R.bundle._3and4._4._4_txt':\n";
-    printAsString(outFile, r4);
+    printAsHash(outFile, r4);
 
     return 0;
 }
